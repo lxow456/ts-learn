a75a35630255bdb6d3287e68d63ea2ad442a2f37
LiXiang <1210548838@qq.com>
2022-02-22 22:16:57 +0800

    feat: delete setter

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,7 @@ class Employee {
     get fullName(): string {
         return this._fullName;
     }
-    // set fullName(newName: string) {
-    //     if (passcode && passcode === 'xxx') {
-    //         this._fullName = newName;
-    //     } else {
-    //         console.log('uncorrect passcode!');
-    //     }
-    // }
 };
 
 let e = new Employee();
-e.fullName
-
-console.log(e.fullName);
+e.fullName;
